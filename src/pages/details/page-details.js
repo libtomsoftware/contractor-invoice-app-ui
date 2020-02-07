759f7488793bd010c5ef847bb46f271fa7b71108
Tomasz Libich <tomaszlibich@Tomaszs-MacBook-Pro.local>
2020-02-07 17:14:53 +0000

    Fix styling

--- src/pages/details/page-details.js
+++ src/pages/details/page-details.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import Details from '../../components/details';
-import Footer from '../../components/common/footer/footer';
-import './page-details.css';
+import React from "react";
+import Details from "../../components/details";
+import Footer from "../../components/common/footer/footer";
+import "./page-details.css";
 
 const PageDetails = () => {
-
-    return (
-        <div className="page page-details">
-            <Details />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="page page-details">
+      <Details />
+      <Footer />
+    </div>
+  );
 };
 
 export default PageDetails;
